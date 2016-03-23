bcbe4a10850e29318622438b0b38170cb5570ead
segee <segee@maine.edu>
2016-03-23 22:37:39 +0000

    Added script to add chrome and chrome remote desktop.

---
+++ install_chrome_and_remote_desktop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
+sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f -y xvfb-randr  xbase-clients python-psutil libappindicator1 
+
+sudo wget http://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo dpkg -i chrome-remote-desktop*.deb
+sudo apt-get -f -y install
+sudo apt-get install -f -y xvfb python-psutil xbase-clients xrandr
+sudo dpkg -i chrome-remote-desktop*.deb
+for USER in '/home/*' 
+   do echo "startxfce4" >>/home/$USER/.chrome-remote-desktop-session;
+done
+echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768">>/etc/profile
