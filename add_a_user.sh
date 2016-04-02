9d34677bed63478d5591ca53e63beff334c9ef55
root <root@localhost.localdomain>
2016-04-02 10:38:40 +0000

    Fixed a sudo issue in add_a_user

--- add_a_user.sh
+++ add_a_user.sh
@@ -17,6 +17,6 @@ echo "Password is $PASSWORD"
 
 sudo useradd -m -s /bin/bash $USERNAME -gsudo
 echo "$USERNAME:$PASSWORD"|sudo chpasswd
-sudo echo "startxfce4" >>/home/$USERNAME/.chrome-remote-desktop-session
-sudo echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768">>/home/$USERNAME/.bashrc
+echo "startxfce4"| sudo tee >>/home/$USERNAME/.chrome-remote-desktop-session
+echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768"|sudo tee>>/home/$USERNAME/.bashrc
 
