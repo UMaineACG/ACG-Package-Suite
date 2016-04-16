12337704f6bbc22b1a62db69bb75d05c18b02c10
segee <segee@maine.edu>
2016-04-16 10:02:51 -0400

    Changed to set ubuntu password

--- starter.sh
+++ starter.sh
@@ -4,6 +4,9 @@ sudo apt-get install -f -y git
 cd /usr/local/bin
 sudo git clone https://github.com/UMaineACG/ACG-Package-Suite.git
 export PATH=$PATH:/usr/local/bin/ACG-Package-Suite:/usr/games:/usr/local/games
+PASSWORD= $(apg -n 1 -Msnc)
+echo "ubuntu:$PASSWORD"|sudo chpasswd
+echo "Password for ubuntu has been set to $PASSWORD"
 cd ACG-Package-Suite
 echo PATH=\"$PATH\"| sudo tee /etc/environment
 for SCRIPT in *.sh
