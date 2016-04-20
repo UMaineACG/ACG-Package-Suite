5f3819d77c1033aa89f58b89d69453452f0ee1da
nh-99 <ifence9@gmail.com>
2016-04-20 12:47:28 -0400

    Re-add add a user

---
+++ add_a_user.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+###################################################################
+# 
+# add a user 
+#
+# call should be of the form 
+# printf "id\npass"|/usr/local/bin/ACG-Package-Suite/add_user.sh
+# where id is username and pass is password (with a \n between)
+#
+###################################################################
+echo "Please enter a user name"
+read USERNAME
+echo "Username is $USERNAME"
+echo "Please enter a password"
+read PASSWORD
+echo "Password is $PASSWORD"
+
+sudo useradd -m -s /bin/bash $USERNAME -gsudo
+echo "$USERNAME:$PASSWORD"|sudo chpasswd
+sudo chage -d 0 $USERNAME
+echo "Password much be changed on first login"
+echo "startxfce4"| sudo tee /home/$USERNAME/.chrome-remote-desktop-session
+echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768"|sudo tee -a /home/$USERNAME/.bashrc
+echo "export TZ=America/New_York" | sudo tee -a /home/$USERNAME/.bashrc
