11ac385d0bc927065146cb51f8f2deaac5a7e12e
nh-99 <nh-99@users.noreply.github.com>
2016-05-04 09:44:31 -0400

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -4,7 +4,9 @@
 user_to_change_pw="ubuntu"
 
 # Check OS type and install git package
-if $(cat /etc/*-release) == *"Ubuntu"*; then
+. /etc/lsb-release
+OS=$DISTRIB_ID
+if $OS == *"Ubuntu"*; then
   sudo apt-get update
   sudo apt-get install -f -y git
 else
