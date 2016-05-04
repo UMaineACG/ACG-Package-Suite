9e10de3be6bfbf3460751087eb5275cc13377b37
nh-99 <ifence9@gmail.com>
2016-05-04 09:14:01 -0400

    Port install desktop environment to CentOS

--- add_a_user.sh
+++ add_a_user.sh
@@ -16,9 +16,9 @@ read PASSWORD
 echo "Password is $PASSWORD"
 
 sudo useradd -m -U -G adm,dialout,cdrom,floppy,sudo,audio,dip,video,plugdev,netdev -s /bin/bash $USERNAME
-echo "$USERNAME:$PASSWORD"|sudo chpasswd
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
 sudo chage -d 0 $USERNAME
 echo "Password much be changed on first login"
-echo "startxfce4"| sudo tee /home/$USERNAME/.chrome-remote-desktop-session
-echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768"|sudo tee -a /home/$USERNAME/.bashrc
+echo "startxfce4" | sudo tee /home/$USERNAME/.chrome-remote-desktop-session
+echo "export CHROME_REMOTE_DESKTOP_DEFAULT_DESKTOP_SIZES=1024x768" | sudo tee -a /home/$USERNAME/.bashrc
 echo "export TZ=America/New_York" | sudo tee -a /home/$USERNAME/.bashrc
---
+++ centos/install_gnome_deskop.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yum groupinstall "GNOME Desktop" "Graphical Administration Tools"
+
+# Run GDM on system boot
+ln -sf /lib/systemd/system/runlevel5.target /etc/systemd/system/default.target
