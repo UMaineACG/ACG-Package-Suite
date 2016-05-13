80733dccbdc104790d8a66266227dc1cfaa89ba7
nh-99 <ifence9@gmail.com>
2016-05-13 00:49:24 -0400

    Fix for starter to make OS check work in 16.04 or systems without python

--- starter.sh
+++ starter.sh
@@ -4,7 +4,7 @@
 user_to_change_pw="ubuntu"
 
 # Check OS type and install git package
-. /etc/lsb-release
+sudo apt-get install python
 OS=$(python -mplatform | grep -Eo Ubuntu)
 if [ "$OS" == "Ubuntu" ]; then
   sudo apt-get update
