a0fc9cc0a7865368b0b755cd7b9f461db793f4f3
nh-99 <ifence9@gmail.com>
2016-05-20 13:47:59 -0400

    Matlab install script

---
+++ ubuntu/install_matlab.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Matlab
+username=$(tempfile 2>/dev/null)
+password=$(tempfile 2>/dev/null)
+mkdir matlab && cd matlab
+dialog --title "UMS Username" --clear --inputbox "Enter your username for your UMS account" 10 30 2> $username
+dialog --title "UMS Password" --clear --passwordbox "Enter your password for your UMS account" 10 30 2> $password
+curl -O -G --data-urlencode "username=$(cat $username)" --data-urlencode "password=$(cat $password)" http://204.197.0.108:5000/files/matlab.zip
+unzip matlab.zip
+sudo ./install
