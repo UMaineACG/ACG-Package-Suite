1b2b419dac13be33338f12e660311618696814b3
Christopher Lewicki <lewicki.christopher@gmail.com>
2016-06-03 13:41:13 -0400

    Passing quiet flags wasn't enough to silence output. Piping output to /dev/null now.

--- install_jupyter.sh
+++ install_jupyter.sh
@@ -8,24 +8,24 @@ fi
 
 
 echo "Updating software sources..."
-apt-get -qq update & # -qq silences all messages except for errors. Pretty cool.
+apt-get -qq update > /dev/null & # -qq silences all messages except for errors. Pretty cool.
 wait
 echo "Software sources updated!"
 
 echo "Installing base dependencies..."
-apt-get -qq install -y python3 python3-pip npm nodejs-legacy &
+apt-get -qq install -y python3 python3-pip npm nodejs-legacy > /dev/null &
 wait
 echo "Base dependencies installed!"
 
 echo "Installing node dependencies..."
-npm --silent install -g configurable-http-proxy &
+npm --silent install -g configurable-http-proxy > /dev/null &
 wait
 echo "Node dependencies installed!"
 
 echo "Installing JupyterHub and JupyterHub Notebook..."
-pip3 -q install jupyterhub &
+pip3 -q install jupyterhub > /dev/null &
 wait
-pip3 -q install --upgrade notebook &
+pip3 -q install --upgrade notebook > /dev/null &
 wait
 echo "JupyterHub and JupyterHub Notebook installed!"
 
