fc539f5270b3cda40a786283dfdc174e3bb6984c
noah <ifence9@gmail.com>
2016-06-08 17:59:40 -0400

    JupyterHub installer

---
+++ ubuntu/install_jupyter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#JupyterHub
+echo "Updating software sources..."
+sudo apt-get -qq update > /dev/null & # -qq silences all messages except for errors. Pretty cool.
+wait
+echo "Software sources updated!"
+
+echo "Installing base dependencies..."
+sudo apt-get -qq install -y python3 python3-pip npm nodejs-legacy openssl > /dev/null &
+wait
+echo "Base dependencies installed!"
+
+echo "Installing node dependencies..."
+sudo npm --silent install -g configurable-http-proxy > /dev/null &
+wait
+echo "Node dependencies installed!"
+
+echo "Installing JupyterHub and JupyterHub Notebook..."
+sudo pip3 -q install jupyterhub > /dev/null &
+wait
+sudo pip3 -q install --upgrade notebook > /dev/null &
+wait
+echo "JupyterHub and JupyterHub Notebook installed!"
+
+printf "\n\n\n"
+
+echo "Configuring JupyterHub..."
+sudo jupyterhub --generate-config
+sudo mkdir -p /var/jupyterhub-data && cd /var/jupyterhub-data
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt
+echo "To start the JupyterHub server, run 'jupyterhub --ip 0.0.0.0 --port 443 --ssl-key /var/jupyterhub-data/server.key --ssl-cert /var/jupyterhub-data/server.crt' as root."
