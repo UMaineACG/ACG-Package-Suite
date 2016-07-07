0d8a563a67a32bace3ba40e57a46e58d7731f811
Christopher Lewicki <lewicki.christopher@gmail.com>
2016-07-06 21:30:43 -0400

    Force enable gdm. Should work just fine now.

--- centos/install_gnome_deskop.sh
+++ centos/install_gnome_deskop.sh
@@ -4,4 +4,4 @@ sudo yum groupinstall -y "GNOME Desktop" "Graphical Administration Tools"
 sudo yum install -y libreoffice
 
 # Run GDM on system boot
-sudo systemctl enable gdm.service
+sudo systemctl -f enable gdm.service
