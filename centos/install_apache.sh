2668497819af3b398563e28fdbced463ad66bf34
noah <ifence9@gmail.com>
2016-07-13 16:01:39 -0400

    Apache installer

---
+++ centos/install_apache.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#Apache Web Server
+sudo yum update -y
+sudo yum install httpd -y
+sudo service httpd start
--- centos/install_mysql.sh
+++ centos/install_mysql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#MySQL
 sudo yum update -y
 sudo rpm -Uvh http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
 sudo yum install -y mysql-server
