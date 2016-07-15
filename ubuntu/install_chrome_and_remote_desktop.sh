93b87ef4b743985e0817187332c02733a15bfba9
segee <segee@maine.edu>
2016-07-15 12:01:21 -0400

    Puts files in /tmp/ACG

--- ubuntu/install_chrome_and_remote_desktop.sh
+++ ubuntu/install_chrome_and_remote_desktop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Google Chrome and Chrome Remote Desktop
+mkdir /tmp/ACG
+cd /tmp/ACG
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb 
 sudo dpkg -i google-chrome*.deb
 sudo apt-get install -f -y xvfb-randr  xbase-clients python-psutil libappindicator1 
