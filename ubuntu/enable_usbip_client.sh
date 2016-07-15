a7647baf585c11e490b99bfae2081a8ecbf1bce5
segee <segee@maine.edu>
2016-07-15 09:27:58 -0400

    fixed writing to /etc/modules with sudo

--- ubuntu/enable_usbip_client.sh
+++ ubuntu/enable_usbip_client.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 #Enable USBIP client
+mod=vhci-hcd
+file=/etc/modules
 
 echo "Adding needed kernel modules..."
-sudo modprobe vhci-hcd
+sudo modprobe $mod
 echo "Kernel modules added!"
 
 echo "But let's check to make sure..."
@@ -10,5 +12,5 @@ sudo lsmod | grep vhci-hcd
 echo "Everything look good? Let's move on."
 
 echo "Adding modules to /etc/modules..."
-sudo echo "vhci-hcd" >> /etc/modules
+grep -Fqx $mod $file || echo $mod |sudo tee -a  $file
 echo "Modules added!"
