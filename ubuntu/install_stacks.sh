746485479a3dbd4e4c1a2e3fd3d2afe83b5f0821
Christopher Lewicki <lewicki.christopher@gmail.com>
2016-09-21 08:28:32 +0000

    finished up the stacks install script

--- ubuntu/install_stacks.sh
+++ ubuntu/install_stacks.sh
@@ -19,7 +19,24 @@ wait
 sudo apt-get install -y mysql-server mysql-client php5 php5-mysqlnd libspreadsheet-writeexcel-perl > /dev/null &
 wait
 echo "Prerequisites installed!"
-cd /usr/local/share/stacks/sql/
-cp mysql.cnf.dist mysql.cnf
+sudo cd /usr/local/share/stacks/sql/
+sudo cp mysql.cnf.dist mysql.cnf
 
 echo "User action required. If you've never used MySQL before on this machine, you can configure it with the following commands: \n mysql \n GRANT ALL ON *.* TO 'stacksuser'@'localhost' IDENTIFIED BY 'stackspassword';\n\nNow edit mysql.cnf to contain the username and password you gave to MySQL\n \n"
+
+read -p "Press any key to continue..." -n1 -s
+
+echo '<Directory "/usr/local/share/stacks/php">
+    Order deny, allow
+    Deny from all
+    Allow from all
+    Require all granted
+</Directory>
+
+Alias /stacks "/usr/local/share/stacks/php"' > stacks.conf
+
+sudo mv stacks.conf /etc/apache2/conf-available && sudo ln -s /etc/apache2/conf-available /etc/apache2/conf-enabled && sudo apachectl restart
+
+sudo cp /usr/local/share/stacks/php/constants.php.dist /usr/local/share/stacks/php/constants.php
+
+echo "Now all that's left to do is edit /usr/local/share/stacks/php/constants.php to include your MySQL information!"
