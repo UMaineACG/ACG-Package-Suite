6d0f985c3e9a0b7715d572940b5f2d7701b9fc0d
noah <ifence9@gmail.com>
2016-11-28 14:59:24 -0500

    New ISPConfig installer

---
+++ ubuntu/install_ispconfig.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ISPConfig Web Panel
+sudo apt-get update && apt-get upgrade
+sudo apt-get install unzip
+cd /tmp
+wget --no-check-certificate https://github.com/servisys/ispconfig_setup/archive/master.zip
+unzip master.zip
+cd ispconfig_setup-master/
+./install.sh
