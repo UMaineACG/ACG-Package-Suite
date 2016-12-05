0eb7eec62792aab626ec5aa84eb3414369992714
nh-99 <ifence9@gmail.com>
2016-12-05 14:26:49 -0500

    Update seafile installer

--- ubuntu/install_seafile.sh
+++ ubuntu/install_seafile.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 #Seafile Client
-export SFCLIENT=seafile_5.1.0_amd64.deb
-mkdir /tmp/ACG
-cd /tmp/ACG
-sudo  apt-get update
-sudo apt-get install -y libjansson4 libqt5test5
-wget https://bintray.com/artifact/download/seafile-org/seafile/$SFCLIENT
-
-sudo dpkg -i $SFCLIENT
-rm *
-cd
-rmdir /tmp/ACG
+sudo add-apt-repository -y ppa:seafile/seafile-client
+sudo apt-get update
+sudo apt-get install -y seafile-gui
