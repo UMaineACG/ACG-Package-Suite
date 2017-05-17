b8e189a1ec57202f3923116454fdc66d65ae83e6
nh-99 <ifence9@gmail.com>
2017-05-17 12:56:16 -0400

    Fixes for tab complete in XRDP

--- ubuntu/install_xrdp.sh
+++ ubuntu/install_xrdp.sh
@@ -8,6 +8,13 @@ for USER in /home/*
         echo xfce4-session > $USER/.xsession
 done
 
+for DIR in /etc/xdg/xfce4/xfconf/xfce-perchannel-xml /etc/xdg/xdg-xubuntu/xfce4/xfconf/xfce-perchannel-xml /home/*/.config/xfce4/xfconf/xfce-perchannel-xml
+    do
+        FILE=$DIR/xfce4-keyboard-shortcuts.xml
+        sudo cp $FILE $FILE.original
+        sudo sed -i -e 's/<property name="\&lt;Super\&gt;Tab" type="string" value="switch_window_key"\/>/<property name="\&lt;Super\&gt;Tab" type="empty"\/>/' $FILE
+done
+
 sudo service xrdp restart
 
 sudo sed -i -e 's/port=-1/port=ask-1/g' /etc/xrdp/xrdp.ini #fun with regex
