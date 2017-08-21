b3924e402f289ceb8726cf97481d2cbc1259a743
Daniel Paradis <dan.paradis@maine.edu>
2017-08-21 15:26:18 -0400

    Added xrdp script and guacamole add script

--- ubuntu/add_connection.sh
+++ ubuntu/add_connection.sh
@@ -4,4 +4,4 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install ansible
-ansible-playbook playbooks/xrdp.yml --ask-sudo-pass
+guac_run
