f3c4e9006630403d6f58714ac3822377d0768298
segee <segee@maine.edu>
2017-11-22 14:19:06 -0500

    Update install_usbip.sh
    Expanded the install_usbip.sh script to also load the kernel modules, start the daemon, and make it happen on every subsequent boot

--- ubuntu/install_usbip.sh
+++ ubuntu/install_usbip.sh
@@ -5,4 +5,19 @@ sudo apt-get install -y linux-image-generic
 sudo apt-get install -y  linux-image-extra-$(uname -r)
 sudo apt-get install -y linux-tools-common
 sudo apt-get install -y linux-tools-$(uname -r)
-
+# Load Kernel modules for client and server
+sudo modprobe usbip-core
+sudo modprobe vhci-hcd
+sudo modprobe usbip-host
+# add them to /etc/modules
+echo 'usbip-core' | sudo tee -a /etc/modules
+echo 'vhci-hcd' | sudo tee -a /etc/modules
+echo 'usbip-host' | sudo tee -a /etc/modules
+# Start the daemon
+sudo usbipd -D
+# add it to /etc/rc.local
+# first delete the last line which is exit 0
+sudo sed -i '$ d' /etc/rc.local
+# now append the line we need to add and append exit 0 after that
+echo 'usbipd -D' |sudo tee -a /etc/rc.local
+echo 'exit 0' |sudo tee -a /etc/rc.local
