cb11b84a8b8dd27e63c63fa7996c221852ad0473
Bruce <segee@maine.edu>
2018-08-08 14:43:44 -0400

    Small tweak to password changing script

--- ubuntu/services/acg_password.sh
+++ ubuntu/services/acg_password.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PASSWORD=\$(date +%s | sha256sum | base32 | head -c 8 ; echo)
+export PASSWORD=$(date +%s | sha256sum | base32 | head -c 8 ; echo)
 echo "acg:\$PASSWORD" | sudo chpasswd 
 echo "Password for acg has been set to $PASSWORD" 
 exit 0
