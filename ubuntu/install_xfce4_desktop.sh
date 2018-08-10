9138a79c1a9e16689e1faf8d9e11d26d567bbb42
segee <segee@maine.edu>
2018-08-10 14:47:59 -0400

    Update install_xfce4_desktop.sh

--- ubuntu/install_xfce4_desktop.sh
+++ ubuntu/install_xfce4_desktop.sh
@@ -11,38 +11,23 @@ sudo locale-gen
 sudo timedatectl set-timezone America/New_York
 sudo apt-get -y  update
 sudo apt-get -y  upgrade 
-# sudo apt-get -y   dist-upgrade
-# sudo apt-get install -y -f grub-legacy-ec2
-# sudo grub-install --recheck /dev/vda
+sudo apt  -y autoremove
+
 sudo apt-get  -y install
-sudo apt-get install -y  -f xubuntu-core
-# sudo /usr/share/debconf/fix_db.pl
-# sudo apt-get -f -y install
-# sudo apt-get install -y -f grub-legacy-ec2
-# sudo apt-get -f -y install
-# sudo grub-install --recheck /dev/vda
-# sudo apt-get -f -y install
-# sudo apt-get install -y -f dictionaries-common
-# sudo apt-get install -y -f libreoffice
-# sudo apt-get install -y -f grub-legacy-ec2
-# sudo grub-install --recheck /dev/vda
-# sudo apt-get -f install
-# sudo apt-get install -y -f dictionaries-common
-# sudo apt-get install -y -f libreoffice
+sudo apt-get install -y  -f xubuntu-desktop
+
+sudo apt-get install -y -f libreoffice
 sudo apt-get install -y  -f man manpages manpages-dev freebsd-manpages funny-manpages gmt-manpages man2html manpages-posix manpages-posix-dev asr-manpages
 sudo apt-get install -f -y xvfb python-psutil xbase-clients xrandr
 sudo apt-get install -f -y  gtk2-engines-pixbuf
 sudo apt-get install -f -y libxss1 libappindicator1 libindicator7 
-# sudo apt-get -f -y install
+sudo apt-get -f -y install
 
 echo "en_US.UTF-8 UTF-8" | sudo tee -a /etc/locale.gen
 sudo locale-gen
 sudo timedatectl set-timezone America/New_York
 echo PATH=\"$PATH\"| sudo tee /etc/environment
 
-
-#sudo echo "America/New_York" | sudo tee /etc/timezone >/dev/null
-#sudo dpkg-reconfigure -f noninteractive tzdata
 /usr/local/bin/ACG-Package-Suite/ubuntu/install_chrome_and_remote_desktop.sh
 /usr/local/bin/ACG-Package-Suite/ubuntu/install_office_suite.sh
 sudo apt-get -y install firefox
