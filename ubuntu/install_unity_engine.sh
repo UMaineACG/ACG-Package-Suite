03f4152534f5cf7e957b735e9470de084746838d
Bruce Segee <segee@maine.edu>
2018-10-20 15:17:02 -0400

    Updated install_unity_engine.sh to match new install method

--- ubuntu/install_unity_engine.sh
+++ ubuntu/install_unity_engine.sh
@@ -3,12 +3,12 @@
 # see https://forum.unity.com/threads/unity-on-linux-release-notes-and-known-issues.350256/
 # for more info and possibly later versions
 mkdir /tmp/ACG
-# don't need the sudo on the next command, but want to get it out of the way before 
-# the download that takes some time
-sudo cd /tmp/ACG
-wget http://beta.unity3d.com/download/2b451a7da81d/unity-editor_amd64-2017.2.0xb6Linux.deb
-sudo dpkg -i unity-editor*.deb
-sudo apt-get install -f -y
-sudo dpkg -i unity-editor*.deb
-sudo apt-get install -f -y mono-complete
+cd /tmp/ACG
+#
 
+sudo apt install gconf-service lib32gcc1 lib32stdc++6 libasound2 libc6 libc6-i386 libcairo2 libcap2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libfreetype6 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libgl1-mesa-glx libglib2.0-0 libglu1-mesa libgtk2.0-0 libnspr4 libnss3 libpango1.0-0 libstdc++6 libx11-6 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxtst6 zlib1g debconf npm build-essentials
+cd /tmp/ACG
+#wget http://beta.unity3d.com/download/2b451a7da81d/unity-editor_amd64-2017.2.0xb6Linux.deb
+wget https://beta.unity3d.com/download/dad990bf2728/UnitySetup-2018.2.7f1
+chmod 755 UnitySetup*
+./UnitySetup*
