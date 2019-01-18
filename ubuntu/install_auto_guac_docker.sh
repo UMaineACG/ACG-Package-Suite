37f98a42cad4404f4b3f85006d10a470a98d4df4
segee <segee@maine.edu>
2019-01-18 13:12:48 -0500

    Added script for installing guacamole and auto_guac

---
+++ ubuntu/install_auto_guac_docker.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# with grateful acknowledgement to:
+#https://github.com/MysticRyuujin/guac-install/blob/master/docker-install.sh
+
+cd /tmp
+mkdir guac_install
+cd -
+cd /tmp/guac_install
+
+
+
+# Version number of Guacamole to install
+GUACVERSION="1.0.0"
+
+# Get script arguments for non-interactive mode
+while [ "$1" != "" ]; do
+    case $1 in
+        -m | --mysqlpwd )
+            shift
+            mysqlpwd="$1"
+            ;;
+        -g | --guacpwd )
+            shift
+            guacpwd="$1"
+            ;;
+    esac
+    shift
+done
+
+# Get MySQL root password and Guacamole User password
+if [ -n "$mysqlpwd" ] && [ -n "$guacpwd" ]; then
+        mysqlrootpassword=$mysqlpwd
+        guacdbuserpassword=$guacpwd
+else
+    echo 
+    while true
+    do
+        read -s -p "Enter a MySQL ROOT Password: " mysqlrootpassword
+        echo
+        read -s -p "Confirm MySQL ROOT Password: " password2
+        echo
+        [ "$mysqlrootpassword" = "$password2" ] && break
+        echo "Passwords don't match. Please try again."
+        echo
+    done
+    echo
+    while true
+    do
+        read -s -p "Enter a Guacamole User Database Password: " guacdbuserpassword
+        echo
+        read -s -p "Confirm Guacamole User Database Password: " password2
+        echo
+        [ "$guacdbuserpassword" = "$password2" ] && break
+        echo "Passwords don't match. Please try again."
+        echo
+    done
+    echo
+     while true
+    do
+        read -s -p "Enter the openstack password for the openstack user" openstackpassword
+        echo
+        read -s -p "Confirm Guacamole User Database Password: " password2
+        echo
+        [ "$openstackpassword" = "$password2" ] && break
+        echo "Passwords don't match. Please try again."
+        echo
+    done
+    echo
+fi
+
+#Install Stuff
+sudo apt-get update
+apt-get -y install docker.io mysql-client wget
+
+# Set SERVER to be the preferred download server from the Apache CDN
+SERVER="http://apache.org/dyn/closer.cgi?action=download&filename=guacamole/${GUACVERSION}"
+
+# Download Guacamole authentication extensions
+wget -O guacamole-auth-jdbc-${GUACVERSION}.tar.gz ${SERVER}/binary/guacamole-auth-jdbc-${GUACVERSION}.tar.gz
+if [ $? -ne 0 ]; then
+    echo "Failed to download guacamole-auth-jdbc-${GUACVERSION}.tar.gz"
+    echo "${SERVER}/binary/guacamole-auth-jdbc-${GUACVERSION}.tar.gz"
+    exit
+fi
+
+tar -xzf guacamole-auth-jdbc-${GUACVERSION}.tar.gz
+
+# Start MySQL
+sudo docker run --restart=always --detach --name=mysql --env="MYSQL_ROOT_PASSWORD=$mysqlrootpassword" --publish 3306:3306 mysql
+
+# Sleep to let MySQL load (there's probably a better way to do this)
+#echo "Waiting 30 seconds for MySQL to load"
+#sleep 30
+# just do other stuff
+sudo apt-get install python-openstackclient
+sudo apt-get install python-pip
+sudo python -m pip install mysql-connector-python
+
+# Create the Guacamole database and the user account
+# SQL Code
+SQLCODE="
+create database guacamole_db; 
+create user 'guacamole_user'@'%' identified by '$guacdbuserpassword'; 
+GRANT SELECT,INSERT,UPDATE,DELETE ON guacamole_db.* TO 'guacamole_user'@'%'; 
+flush privileges;"
+
+# Execute SQL Code
+echo $SQLCODE | mysql -h 127.0.0.1 -P 3306 -u root -p$mysqlrootpassword
+
+cat guacamole-auth-jdbc-${GUACVERSION}/mysql/schema/*.sql | mysql -u root -p$mysqlrootpassword -h 127.0.0.1 -P 3306 guacamole_db
+
+sudo docker run --restart=always --name guacd -d guacamole/guacd
+sudo docker run --restart=always --name guacamole  --link mysql:mysql --link guacd:guacd -e MYSQL_HOSTNAME=127.0.0.1 -e MYSQL_DATABASE=guacamole_db -e MYSQL_USER=guacamole_user -e MYSQL_PASSWORD=$guacdbuserpassword --detach -p 8080:8080 guacamole/guacamole
+
+sudo rm -rf guacamole-auth-jdbc-${GUACVERSION}*
+cd -
+sudo mkdir /etc/auto_guac
+cd /etc/auto_guac
+sudo git clone https://github.com/segee/auto_guac
+sudo git clone https://gogs.acg.maine.edu/Openstack_shell_scripts
+cd -
+cd /etc/auto_guac/Openstack_shell_scripts
+echo "echo $openstackpassword > my_password" | sudo bash
+sudo chmod 000 my_password
+cd -
+
+sudo cp /etc/auto_guac/auto_guac/auto_guac_for_docker.service /etc/systemd/system/.
+sudo systemctl enable auto_guac_for_docker.service
+echo 
+echo
+echo
+echo "This is very important"
+echo "we are almost done"
+echo "but not quite"
+echo "A web browser will launch and take you to the"
+echo "guacamole instance you just installed"
+echo
+echo "please log in with the default credentials"
+echo "guacadmin guacadmin"
+echo "and change the password"
+echo "feel free to add a user account for yourself as well"
+echo
+echo
+echo "CLOSE THE BROWSER WHEN DONE"
+read -rsp $'Press any key to continue...\n' -n1 key
+firefox http://localhost:8080/guacamole
+echo
+echo
+echo
+echo "Last step..."
+echo "Log in to openstack and download the RC file for the project"
+echo "go to project->access and security->API access"
+echo "and click Download OpenStack RC file"
+read -rsp $'Press any key to continue...\n' -n1 key
+sudo cp ~/Downloads/*openrc.sh /etc/auto_guac/Openstack_shell_scripts/.
