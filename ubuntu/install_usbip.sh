9539a9663ed85d76160709c3768506c2720dd3fd
segee <segee@maine.edu>
2019-01-22 13:42:48 -0500

    fix to install_usbip.sh

--- ubuntu/install_usbip.sh
+++ ubuntu/install_usbip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #USB IP (Connect USB to server)
 sudo apt-get update
-#sudo apt-get install -y linux-image-generic
+sudo apt-get install -y linux-image-generic
 #sudo apt-get install -y  linux-image-extra-$(uname -r)
 sudo apt-get install -y linux-tools-common
 sudo apt-get install -y linux-tools-generic
