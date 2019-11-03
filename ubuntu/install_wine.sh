9b55a8410be95be87e2e61eae6473b83c22b426f
segee <segee@maine.edu>
2019-11-03 14:43:50 -0500

    Update install_wine.sh

--- ubuntu/install_wine.sh
+++ ubuntu/install_wine.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Install WINE (Windows emulator)    
     sudo dpkg --add-architecture i386
-    wget https://dl.winehq.org/wine-builds/Release.key
-    sudo apt-key add Release.key
-    sudo apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+    wget -qO - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+    sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
     sudo apt-get update
-    sudo apt-get install -y -f winehq-stable winetricks 
+    sudo apt-get install --install-recommends -y -f winehq-stable winetricks 
     winetricks
