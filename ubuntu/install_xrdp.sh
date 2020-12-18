932d743dd072e460ceec648948821d93405b5184
Bruce Segee <segee@maine.edu>
2020-12-18 14:57:00 -0500

    xrdp

--- ubuntu/install_xrdp.sh
+++ ubuntu/install_xrdp.sh
@@ -13,16 +13,16 @@ mkdir /tmp/installxrdp
 cd /tmp/installxrdp
 
 
-curl http://www.c-nergy.be/downloads/xrdp-installer-1.2.zip --output xrdp.zip
+curl http://www.c-nergy.be/downloads/xrdp-installer-1.2.1.zip --output xrdp.zip
 unzip xrdp.zip 
-chmod +x xrdp-installer-1.2.sh 
-sed -i 's/be.archive/us.archive/' xrdp-installer-1.2.sh
+chmod +x xrdp-installer-1.2.1.sh 
+sed -i 's/be.archive/us.archive/' xrdp-installer-1.2.1.sh
 
 if [ "$1" = "-r" ] ;
 then
-	./xrdp-installer-1.2.sh -r
+	./xrdp-installer-1.2.1.sh -r
 else
-	 eval "./xrdp-installer-1.2.sh -s $@"
+	 eval "./xrdp-installer-1.2.1.sh -s $@"
 fi
 
 #./xrdp-installer-1.2.sh  -c -s
