cfea01ad770279e3d91254054057f52cbc034790
segee <segee@maine.edu>
2020-12-31 10:02:31 -0500

    Updates to xrdp scrit to clone git repository recursively as recommended

--- ubuntu/install_xrdp.sh
+++ ubuntu/install_xrdp.sh
@@ -7,25 +7,24 @@
 
 sudo apt-get install python3-pip
 sudo pip3 install apt-mirror-updater
-apt-mirror-updater -a
+#apt-mirror-updater -a
 
 mkdir /tmp/installxrdp
 cd /tmp/installxrdp
 
 
-curl http://www.c-nergy.be/downloads/xrdp-installer-1.2.zip --output xrdp.zip
+curl http://www.c-nergy.be/downloads/xrdp-installer-1.2.1.zip --output xrdp.zip
 unzip xrdp.zip 
-chmod +x xrdp-installer-1.2.sh 
-sed -i 's/be.archive/us.archive/' xrdp-installer-1.2.sh
-
+chmod +x xrdp-installer-1.2.1.sh 
+sed -i 's/be.archive/us.archive/' xrdp-installer-1.2.1.sh
+sed -i 's/git clone/git clone --recursive/' xrdp-installer-1.2.1.sh
 if [ "$1" = "-r" ] ;
 then
-	./xrdp-installer-1.2.sh -r
+	./xrdp-installer-1.2.1.sh -r
 else
-	 eval "./xrdp-installer-1.2.sh -s $@"
+	 eval "./xrdp-installer-1.2.1.sh -s $@"
 fi
 
-#./xrdp-installer-1.2.sh  -c -s
 
 cd /tmp/installxrdp
 rm *
