1a1cbf012faa434f682e8b6fa2d3988b87c52435
segee <segee@maine.edu>
2021-01-02 16:19:45 -0500

    Install onedrive

--- ubuntu/install_onedrive.sh
+++ ubuntu/install_onedrive.sh
@@ -17,7 +17,5 @@ echo " There are good directions here: https://www.linuxuprising.com/2020/02/how
 echo
 echo " A word of caution is that the default is to synchronize all files (could be a lot)"
 echo " After setting up onedrive, and before syncing, create a file in ~/.config/onedrive named sync_list"
- 16:14:34 up 14 days,  1:25,  1 user,  load average: 0.00, 0.06, 0.03
-USER     TTY      FROM             LOGIN@   IDLE   JCPU   PCPU WHAT
-bruce    :1       :1               Sun16   ?xdm?   3:17m  0.01s /usr/lib/gdm3/gdm-x-session --run-script env GNOME_SHELL_SESSION_MODE=ubuntu /usr/bin/gnome-session --systemd --session=ubuntu
+echo " list the onedrive folders you wish to sync, one per line in that file"
 echo "***********************************************"
