ac726e4b0f09bc139bbf8f81db20bcd0b97110e5
segee <segee@maine.edu>
2022-08-02 15:06:37 -0400

    Update install_xrdp.sh
    script version 1.4.2 supports ubuntu 22.04

--- ubuntu/install_xrdp.sh
+++ ubuntu/install_xrdp.sh
@@ -13,20 +13,20 @@ mkdir /tmp/installxrdp
 cd /tmp/installxrdp
 
 
-curl http://www.c-nergy.be/downloads/xrdp-installer-1.2.2.zip --output xrdp.zip
+curl http://www.c-nergy.be/downloads/xrdp-installer-1.4.2.zip --output xrdp.zip
 unzip xrdp.zip 
-chmod +x xrdp-installer-1.2.2.sh 
-sed -i 's/be.archive/us.archive/' xrdp-installer-1.2.2.sh
-sed -i 's/git clone/git clone --recursive/' xrdp-installer-1.2.2.sh
+chmod +x xrdp-installer-1.4.2.sh 
+sed -i 's/be.archive/us.archive/' xrdp-installer-1.4.2.sh
+sed -i 's/git clone/git clone --recursive/' xrdp-installer-1.4.2.sh
 
 if [ "$1" = "-r" ] ;
 then
-	./xrdp-installer-1.2.2.sh -r
+	./xrdp-installer-1.4.2.sh -r
 else
-	 eval "./xrdp-installer-1.2.2.sh -s $@"
+	 eval "./xrdp-installer-1.4.2.sh -s $@"
 fi
 
 
 cd /tmp/installxrdp
 rm *
-echo "pulseaudio -k" |sudo tee -a /etc/profile
+# echo "pulseaudio -k" |sudo tee -a /etc/profile
